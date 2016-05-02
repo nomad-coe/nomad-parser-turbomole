fc35ad7238cfcbee52858fff655f31b1fa156852
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-05-02 16:53:29 +0200

    Fix name in test

--- src/test/scala/eu/nomad_lab/parsers/TurbomoleParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/TurbomoleParserSpec.scala
@@ -3,12 +3,12 @@ package eu.nomad_lab.parsers
 import org.specs2.mutable.Specification
 
 object TurbomoleParserSpec extends Specification {
-  "CastepParserTest" >> {
+  "TurbomoleParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(CastepParser, "parsers/castep/test/examples/Si2_bandstructure/B3LYP/Si2.castep_b_sp_v_1", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(TurbomoleParser, "parsers/turbomole/test/examples", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(CastepParser, "parsers/castep/test/examples/Si2_bandstructure/B3LYP/Si2.castep_b_sp_v_1", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(TurbomoleParser, "parsers/turbomole/test/examples", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
