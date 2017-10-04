e6733a334ced83412d589a14a9438213ee4b3943
Arvid Ihrig <ihrig@fhi-berlin.mpg.de>
2017-10-04 17:30:22 +0200

    tests updated to Scalatest, now parse all example files in the folder
    -the G0W0 file will fail the test due to unclosed sections

--- src/test/scala/eu/nomad_lab/parsers/TurbomoleParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/TurbomoleParserSpec.scala
@@ -1,14 +1,25 @@
 package eu.nomad_lab.parsers
 
-import org.specs2.mutable.Specification
+import java.nio.file.Paths
 
-object TurbomoleParserSpec extends Specification {
-  "TurbomoleParserTest" >> {
-    "test with json-events" >> {
-      ParserRun.parse(TurbomoleParser, "parsers/turbomole/test/examples/NO_2_UHF.out", "json-events") must_== ParseResult.ParseSuccess
-    }
-    "test with json" >> {
-      ParserRun.parse(TurbomoleParser, "parsers/turbomole/test/examples/NO_2_UHF.out", "json") must_== ParseResult.ParseSuccess
+import org.scalatest.{ Matchers, WordSpec }
+
+class TurbomoleParserSpec extends WordSpec with Matchers {
+  "the Turbomole Parser" when {
+    val testFiles = Paths.get("test", "examples").toFile.listFiles().filter(_.toString.endsWith(".out"))
+    val basePath = Paths.get("parsers", "turbomole")
+    testFiles.foreach { file =>
+      val filePath = basePath.resolve(file.toPath)
+      "parsing '" + file.toString + "'" should {
+        "return parsing results as json-events" in {
+          val result = ParserRun.parse(TurbomoleParser, filePath.toString, "json-events")
+          result should be(ParseResult.ParseSuccess)
+        }
+        "return parsing results as json" in {
+          val result = ParserRun.parse(TurbomoleParser, filePath.toString, "json")
+          result should be(ParseResult.ParseSuccess)
+        }
+      }
     }
   }
 }
